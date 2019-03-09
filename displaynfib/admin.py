448596bc3c7e1973e387fc3a0f319b4f5e01447a
Rajath C S <rajathcs.1996@gmail.com>
2019-03-09 16:59:56 +0530

    include django admin page

--- displaynfib/admin.py
+++ displaynfib/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-'''from .models import FibOutput # including the app in the admin panel
-admin.site.register(FibOutput)'''
+from .models import FibOutput # including the app in the admin pane
+admin.site.register(FibOutput)
