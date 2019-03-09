bd14f3b4a1539becf31d18a5266396721784827a
Rajath C S <rajathcs.1996@gmail.com>
2019-03-09 17:01:28 +0530

    append comments

--- displaynfib/models.py
+++ displaynfib/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
 class FibOutput(models.Model):
-    number = models.IntegerField() 
-    result = models.IntegerField()
-    time_taken = models.CharField(max_length=255)
-'''    result = models.PositiveIntegerField() # this is the result seen in 'Nth Fibonacci Number' label
-    number = models.PositiveIntegerField() # this is the input number as provied by the user
-    time_taken = models.CharField(max_length=255) # time consumed to provide output'''
-    
+    number = models.IntegerField()  #this is the input number as provied by the user
+    result = models.IntegerField() # this is the result seen in 'Nth Fibonacci Number' text lab
+    time_taken = models.CharField(max_length=255) # time consumed to provide output
+   
 def __str__(self):
     return self.number  #throwing the output
